bd7ad61ae3802f5c70f34f7ef0166eaac1af4bc7
hgw <j2160204@gmail.com>
2016-08-27 00:01:11 +0800

    finish tutorial

---
+++ js/game.js
@@ -0,0 +1,47 @@
+
+/* Game namespace */
+var game = {
+
+    // an object where to store game information
+    data : {
+        // score
+        score : 0
+    },
+
+
+    // Run on page load.
+    "onload" : function () {
+        // Initialize the video.
+        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto"})) {
+            alert("Your browser does not support HTML5 canvas.");
+            return;
+        }
+
+        // add "#debug" to the URL to enable the debug Panel
+        if (me.game.HASH.debug === true) {
+            window.onReady(function () {
+                me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
+            });
+        }
+
+        // Initialize the audio.
+        me.audio.init("mp3,ogg");
+
+        // set and load all resources.
+        // (this will also automatically switch to the loading screen)
+        me.loader.preload(game.resources, this.loaded.bind(this));
+    },
+
+    // Run on game resources loaded.
+    "loaded" : function () {
+        me.pool.register("player", game.Player);
+        me.pool.register("enemy", game.Enemy);
+        me.pool.register("laser", game.Laser);
+
+        this.playScreen = new game.PlayScreen();
+        me.state.set(me.state.PLAY, this.playScreen);
+        
+        // Start the game.
+        me.state.change(me.state.PLAY);
+    }
+};
